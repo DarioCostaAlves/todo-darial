6b3bcc7bd61ea9e91d2469fc9d8a9493e824dbb4
DarioCostaAlves <dario.c.alves22@gmail.com>
2023-11-17 23:08:24 +0000

    JSX example

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-sm">React Appa</h1>
+      <div>
+        <h1 className="text-5xl text-blue-400 font-mono">React App</h1>
+        <form>
+          <input type="text" placeholder="Enter your name" />
+          <span>Time: {new Date().toLocaleTimeString()}</span>
+        </form>
+      </div>
     </>
   );
 }
